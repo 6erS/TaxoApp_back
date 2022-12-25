a8ac99cd28c6b764ac521a6e21e1dd8b3ce10884
6erS <maksim.huzhelia@gmail.com>
2022-12-25 17:36:24 +0300

    Update registrValidation.js

--- Validations/registrValidation.js
+++ Validations/registrValidation.js
@@ -12,8 +12,8 @@ export const driverValidation = [
     header('firstname', 'Invalid First Name. It must be longer then 2 ch.').isLength({ min: 2 }),
     header('secondname', 'Invalid Second Name. It must be longer then 2 ch.').isLength({ min: 2 }),
     header('password', 'Invalid Password. It must be longer then 8 ch.').isLength({ min: 8 }),
-    header('driverlicid', 'Invalid ID of Driver License. It must be 17 char length.').isLength({min:10, max:10}),
-    header('vin', 'Invalid VIN code. It must be 10 char length.').isLength({min:17, max:17})
+    header('driverlicid', 'Invalid ID of Driver License. It must be 9 char length.').isLength({min:9, max:9}),
+    header('vin', 'Invalid VIN code. It must be 17 char length.').isLength({min:17, max:17})
 ];
 
 export const loginValidation = [
