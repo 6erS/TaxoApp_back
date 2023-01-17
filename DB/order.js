53f55f142f856f5ef164035f5e50570f758d55c4
6erS <maksim.huzhelia@gmail.com>
2023-01-17 12:38:05 +0300

    OrderObj logic

--- DB/order.js
+++ DB/order.js
@@ -2,14 +2,15 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const orderSchema = new Schema({
-    driverPhoneNumber: { type: String, unique: true, require: true },
-    passengerPhoneNumber: { type: String, unique: true, require: true },
+    driverID: { type: String, require: true },
+    passangerID: { type: String, require: true },
     driverFirstName: { type: String, require: true },
     passengerFirstName: { type: String, require: true },
     orderStatus: { type: String, require: true },
-    coordinates: [{  latitude: { type: Number, require: true, default: 0 },
-                longitude: { type: Number, require: true, default: 0 } }],
-    distance: { type: Number, require: true }
+    startAddress: { type: String, require: true },
+    endAddress: { type: String, require: true },
+    distance: { type: Number, require: true },
+    price: { type: Number, require: true },
 });
 
 export const Order = mongoose.model('Order', orderSchema);
